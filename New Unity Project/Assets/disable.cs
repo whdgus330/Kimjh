2da4ec31f89a8ab66b6a51839919d5990f2e6dae
whdgus330 <whdgus332@naver.com>
2017-07-12 07:45:19 +0900

    유니티
    버튼 디스에이블

---
+++ New Unity Project/Assets/disable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disable : MonoBehaviour {
+    public GameObject Disable;
+    public void OnDisable()
+    {
+        Disable.SetActive(false);
+    }
+
+    void Start()
+    {
+        StartCoroutine(Enable());
+    }
+
+    IEnumerator Enable()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(10f);
+        }
+    }
+}
---
+++ New Unity Project/Assets/scene1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class scene1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void click()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
